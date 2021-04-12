ee42c1a45849d8948b0446b7370c34736241fbd8
dhav015 <77419623+dhav015@users.noreply.github.com>
2021-04-12 16:39:16 -0400

    Update email.php

--- Implementation/email.php
+++ Implementation/email.php
@@ -1,18 +1,34 @@
 <?php 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
-if (isset($_POST['submit'])) {
-    $to = $_POST['email']; 
-    $subject = 'Verification Code';
-    $message = 'User has forgotten username/password. Use the link below and the provided verification code to set a new password.';
-    $from = "teamrocket@gmail.com";
-    $headers = "From:" . $from;
+require_once __DIR__ . '/PHPMailer-master/src/Exception.php';
+require_once __DIR__ . '/PHPMailer-master/src/PHPMailer.php';
+require_once __DIR__ . '/PHPMailer-master/src/SMTP.php';
 
-    if (mail($to, $subject, $message, $headers)) {
-        echo "Email sent";
-    }
-    else {
-        echo "Could not send email";
-    }
+$mail = new PHPMailer(true);
+$mail->isSMTP();
+
+$to = $_POST['email'];
+
+$mail->SMTPAuth = true;
+$mail->SMTPSecure = "tls";
+$mail->Port = 587;
+$mail->Host = "smtp.gmail.com";
+$mail->Username = "team2021rocket@gmail.com";
+$mail->Password = "Team@rocket2021";
+
+$mail->setFrom('team2021rocket@gmail.com', 'Team Rocket');
+$mail->addAddress($to);
+
+$mail->IsHTML(true);
+$mail->Subject = "Testing";
+$mail->Body = "User has forgotten username/password. Use the link below and the provided verification code to set a new password.";
+
+if ($mail->send()){
+    echo 'Email sent.';
+} else {
+    echo 'Email not sent.';
 }
-
 ?>
