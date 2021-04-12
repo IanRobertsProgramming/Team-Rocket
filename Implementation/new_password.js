42d3a683feb5de9d7c298ff923f423e50ca01f75
dhav015 <77419623+dhav015@users.noreply.github.com>
2021-04-11 21:33:46 -0400

    new password

---
+++ Implementation/new_password.js
@@ -0,0 +1,27 @@
+function new_password() {
+    var new_pass = $('#new_pass').val();
+    var confirm_pass = $('#confirm_pass').val();
+    var ver_code = sessionStorage.getItem("key");
+
+    $.ajax({
+        type: 'POST',
+        url: '../php/new_password.php',
+        data: { ver_code: ver_code, new_pass: new_pass, confirm_pass: comfirm_pass},
+        success: function(result) {
+            if (result === 'True') {
+                sessionStorage.removeItem("key");
+                var hrefL = "pages/update_successful.html";
+                window.location.replace(hrefL);
+            } else {
+                displayPassChangeError()
+                $('#error').text(result);
+            }
+        }, error: function (result) {
+           alert('failed');
+        }
+    });
+}
+
+function displayPassChangeError() {
+    $('#error').text("Error: Password could not be changed.");
+}
---
+++ Implementation/new_password.php
@@ -0,0 +1,39 @@
+<?php 
+
+    $ver_code = $_POST['ver_code'];
+    $new_pass = $_POST['new_pass'];
+    $confirm_pass = $_POST['confirm_pass'];
+
+    if ($new_pass == $confirm_pass) {
+
+        $username = 'rocket';
+        $password = 'Rocket_7';
+
+        $conn = new mysqli("hopper.wlu.ca", $username, $password, "rocket");
+
+        $stmt = $conn->prepare("SELECT * FROM user WHERE password=?");
+
+        $stmt->bind_param("s", $ver_code); 
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if ($result->num_rows > 0) {
+            $user = $result->fetch_assoc();
+            if ($user) {
+                if ($user['password'] == $ver_code) {
+                    $stmt = $conn->prepare("INSERT INTO user (password) VALUES (?)");
+                    $stmt->bind_param("s", $new_pass);
+                    $stmt->execute();
+                    $result = $stmt->get_result();
+                    $conn->close();
+                    echo 'Password successfully changed';
+                }
+            }
+        } else {
+            echo 'False';
+        }
+    } else {
+        echo 'Passwords don't match';
+    }
+
+?>
