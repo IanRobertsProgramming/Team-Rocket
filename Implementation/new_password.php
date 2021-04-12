65fe5a841eec8430630655495a338a65ca6f6ac8
dhav015 <77419623+dhav015@users.noreply.github.com>
2021-04-11 22:10:47 -0400

    new password

--- Implementation/new_password.php
+++ Implementation/new_password.php
@@ -4,36 +4,31 @@
     $new_pass = $_POST['new_pass'];
     $confirm_pass = $_POST['confirm_pass'];
 
-    if ($new_pass == $confirm_pass) {
+    $username = 'rocket';
+    $password = 'Rocket_7';
 
-        $username = 'rocket';
-        $password = 'Rocket_7';
+    $conn = new mysqli("hopper.wlu.ca", $username, $password, "rocket");
 
-        $conn = new mysqli("hopper.wlu.ca", $username, $password, "rocket");
+    $stmt = $conn->prepare("SELECT * FROM user WHERE password=?");
 
-        $stmt = $conn->prepare("SELECT * FROM user WHERE password=?");
+    $stmt->bind_param("s", $ver_code); 
+    $stmt->execute();
+    $result = $stmt->get_result();
 
-        $stmt->bind_param("s", $ver_code); 
-        $stmt->execute();
-        $result = $stmt->get_result();
-
-        if ($result->num_rows > 0) {
-            $user = $result->fetch_assoc();
-            if ($user) {
-                if ($user['password'] == $ver_code) {
-                    $stmt = $conn->prepare("INSERT INTO user (password) VALUES (?)");
-                    $stmt->bind_param("s", $new_pass);
-                    $stmt->execute();
-                    $result = $stmt->get_result();
-                    $conn->close();
-                    echo 'Password successfully changed';
-                }
+    if ($result->num_rows > 0) {
+        $user = $result->fetch_assoc();
+        if ($user) {
+            if ($user['password'] == $ver_code) {
+                $stmt = $conn->prepare("INSERT INTO user (password) VALUES (?)");
+                $stmt->bind_param("s", $new_pass);
+                $stmt->execute();
+                $result = $stmt->get_result();
+                $conn->close();
+                echo 'Password successfully changed';
             }
-        } else {
-            echo 'False';
         }
     } else {
-        echo 'Passwords don't match';
+        echo 'False';
     }
 
 ?>
