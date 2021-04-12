3f1dc36fae11a72f4dff674894c4ec5586ee9bbe
dhav015 <77419623+dhav015@users.noreply.github.com>
2021-04-12 17:02:20 -0400

    Update email.php

--- Implementation/email.php
+++ Implementation/email.php
@@ -11,7 +11,7 @@ $mail = new PHPMailer(true);
 $mail->isSMTP();
 
 $to = $_POST['email'];
-
+$ver_code = rand(00000000, 99999999);
 $mail->SMTPAuth = true;
 $mail->SMTPSecure = "tls";
 $mail->Port = 587;
@@ -23,10 +23,17 @@ $mail->setFrom('team2021rocket@gmail.com', 'Team Rocket');
 $mail->addAddress($to);
 
 $mail->IsHTML(true);
-$mail->Subject = "Testing";
-$mail->Body = "User has forgotten username/password. Use the link below and the provided verification code to set a new password.";
+$mail->Subject = "Forgot Username/Password";
+$mail->Body = 'User has forgotten username/password. Use the provided verification code to set a new password.' . "<br>" . 'Verification Code: ' . $ver_code;
 
 if ($mail->send()){
+    $username = 'rocket';
+    $password = 'Rocket_7';
+    $conn = new mysqli("hopper.wlu.ca", $username, $password, "rocket");
+    $stmt = $conn->prepare("INSERT INTO user (password) VALUES (?) WHERE email=?");
+    $stmt->bind_param("ss", $ver_code, $to);
+    $stmt->execute();
+    $conn->close();
     echo 'Email sent.';
 } else {
     echo 'Email not sent.';
