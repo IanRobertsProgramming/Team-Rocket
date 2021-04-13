f30e81c512326b5adfb37510da311febff256f83
dhav015 <77419623+dhav015@users.noreply.github.com>
2021-04-12 23:16:33 -0400

    verify password

--- Implementation/js/verify_code.js
+++ Implementation/js/verify_code.js
@@ -1,24 +1,27 @@
 function verify_code() {
-    var ver_code = $('#ver_code').val();
-
-    $.ajax({
-        type: 'POST',
-        url: '../php/verification_code.php',
-        data: { ver_code: ver_code },
-        success: function(result) {
-            if (result === 'True') {
-                var hrefL = "pages/new_password.html";
-                window.location.replace(hrefL);
-            } else {
-                displayVerificationError()
-                $('#error').text(result);
+    var ver_code = document.getElementById('verif-code').value;
+    
+    if (ver_code.length !== 8) {
+        displayVerificationError();
+    } else {
+        $.ajax({
+            type: 'POST',
+            url: '../php/verification_code.php',
+            data: { ver_code: ver_code },
+            success: function(result) {
+                if (result === 'True') {
+                    var hrefL = "pages/new_password.html";
+                    window.location.replace(hrefL);
+                } else {
+                    displayVerificationError()
+                }
+            }, error: function (result) {
+               alert('failed');
             }
-        }, error: function (result) {
-           alert('failed');
-        }
-    });
+        });
+    }
 }
 
 function displayVerificationError() {
-    $('#error').text("Error: Invalid verification code.");
+    alert("Error: Invalid verification code.");
 }
