296377ef93f57b9e1b0e6d4cab736153d361c01e
Scott Vodon <scottyvodon@gmail.com>
2021-04-15 23:49:08 -0400

    Settings Update

--- Documentation/public/settings.js
+++ Documentation/public/settings.js
@@ -1,49 +1,75 @@
-function getSettingsInfo(){
-$.ajax({
-    type: 'post',
-    url: '/rocket/php/settings.php',
-    document.cookie
-    data: { user: username, pass: password},
-    success: function(result) {
-        if (result === 'True') {
-            var hrefL = "main-page.html";
-            window.location.replace(hrefL);
-        } else {
-            displayLoginError();
+jQuery(document).ready(function ($) {
+
+    $(".EditButton button").on("click",function(){
+      $(".EditButton button").hide()
+      $(".SubButton button").show()
+    })
+
+    $(".EditButton2 button").on("click",function(){
+      $(".Username p").hide()
+      $(".Username input").show()
+      $(".EditButton2 button").hide()
+      $(".SubButton2 button").show()
+    })
+
+    $(".SubButton2 button").on("click",function(){
+      $(".Username p").show()
+      $(".Username input").hide()
+      $(".EditButton2 button").show()
+      $(".SubButton2 button").hide()
+    })
+
+    $(".EditButton3 button").on("click",function(){
+      $(".Email p").hide()
+      $(".Email input").show()
+      $(".EditButton3 button").hide()
+      $(".SubButton3 button").show()
+    })
+
+    function getSettingsInfo(){
+    $.ajax({
+        type: 'post',
+        url: '/rocket/php/settings.php',
+        //document.cookie,
+        data: { user: username, pass: password},
+        success: function(result) {
+            if (result === 'True') {
+                var hrefL = "main-page.html";
+                window.location.replace(hrefL);
+            } else {
+                displayLoginError();
+            }
+        }, error: function (result) {
+            alert('Connection Failed');
         }
-    }, error: function (result) {
-        alert('Connection Failed');
+    });
+
+    }
+    function changeUser(){
+        var new_username = $('#new_username').val();
+        var change = 1;
+        $.ajax({
+            type: 'POST',
+            url: '/rocket/php/change_info.php',
+            data: { ver_code: ver_code, new_pass: new_user, change: change},
+            success: function(result) {
+            }, error: function (result) {
+                alert('Connection Failed');
+            }
+        });
+    }
+
+    function changeEmail(){
+        var new_email = $('#new_email').val();
+        var change = 2;
+        $.ajax({
+            type: 'POST',
+            url: '/rocket/php/change_info.php',
+            data: { ver_code: ver_code, new_pass: new_user, change: change},
+            success: function(result) {
+            }, error: function (result) {
+                alert('Connection Failed');
+            }
+        });
     }
 });
-
-}
-
-function changeUser(){
-    var new_username = $('#new_username').val();
-    var change = 1;
-    $.ajax({
-        type: 'POST',
-        url: '/rocket/php/change_info.php',
-        data: { ver_code: ver_code, new_pass: new_user, change: change},
-        success: function(result) {
-        }, error: function (result) {
-            alert('Connection Failed');
-        }
-    });
-}
-}
-
-function changeEmail(){
-    var new_email = $('#new_email').val();
-    var change = 2;
-    $.ajax({
-        type: 'POST',
-        url: '/rocket/php/change_info.php',
-        data: { ver_code: ver_code, new_pass: new_user, change: change},
-        success: function(result) {
-        }, error: function (result) {
-            alert('Connection Failed');
-        }
-    });
-}
-}
