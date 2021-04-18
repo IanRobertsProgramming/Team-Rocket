2f4d346f1d9a4e963a515e905544df1b159e4583
Carson-Bunk <64325281+Carson-Bunk@users.noreply.github.com>
2021-04-18 16:53:33 -0400

    added php files that query the database for userID username and password

---
+++ Documentation/php/display-email.php
@@ -0,0 +1,22 @@
+<?php
+    $userID = $_COOKIE['PkmUserID'];
+    $new_info = $_POST['new_info'];
+    $change = $_POST['change']
+
+    $username = 'rocket';
+    $password = 'Rocket_7';
+
+    $conn = new mysqli("hopper.wlu.ca", $username, $password, "rocket");
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+      }
+    
+      
+    $sql = "SELECT email FROM user WHERE userID=?";
+    $result = $conn->query($sql);
+    echo $result
+    
+    $conn->close();
+
+?>
